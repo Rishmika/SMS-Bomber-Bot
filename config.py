5093ac6f5430c4dc709a6a8c208a67bcf2f60800
DɪʟᴜᴍBBᴀɴᴅᴀʀᴀ <104001186+DilumBBandara@users.noreply.github.com>
2022-11-02 18:45:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-bot_username = 'Mars11Lkbot'
+bot_username = ''
 CHANNEL = 'Team_Mars_11'
 Discussion = 'TeamMars_11'
 C_CHANNEL = 'CGSUPDATES'
@@ -8,11 +8,11 @@ SUPPORT = 'CGSsupport'
 
 start_sticker_id = 'CAACAgIAAxkBAAIBP2NK3cJnHKz9Tz0N1XY7XnQfnP6iAAJlCwACUas4S_UAAVke-cEmqioE'
 
-TG_API_HASH = '9d2c6cdf712fc6cd9e667567111a1cb8'
-TG_API_ID = '9247680'
-TG_BOT_TOKEN = '5677621036:AAGx5MalG9-DYun-KCQoj9EB795tr2_utz0'
+TG_API_HASH = ""
+TG_API_ID = ""
+TG_BOT_TOKEN = ""
 
-OWNER = int(5262156299) #oyage id danna
-CO_OWNER = int(1746346543)
+OWNER = int() #oyage id danna
+CO_OWNER = int()
 
-DATABASE = "mongodb+srv://Dilumb:dilumb12@cluster0.x2zfbab.mongodb.net/?retryWrites=true&w=majority" #Mongo url
+DATABASE = "" #Mongo url
