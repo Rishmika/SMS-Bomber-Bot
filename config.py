bc5f59136987ef245a74914faced3dbd838eb446
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-11-11 20:14:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ SUPPORT = 'EmoBotSupport'
 
 start_sticker_id = 'CAACAgIAAxkBAAIBP2NK3cJnHKz9Tz0N1XY7XnQfnP6iAAJlCwACUas4S_UAAVke-cEmqioE'
 
-TG_API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
-TG_API_ID = "14055090"
-TG_BOT_TOKEN = "5688863352:AAEMU2_Dd3wN8dQQ31ixq78mF3rHeXXfsuM"
+TG_API_HASH = "a46f7b439xxxxxxxx7a69fc450f754e9"
+TG_API_ID = "14xxxxx"
+TG_BOT_TOKEN = "5688863352:xxxxx2_Dd3wN8dQQ31ixq78mF3rHeXXfsuM"
 
 OWNER = int(5171347305) #oyage id danna
 CO_OWNER = int(5098417240)
 
-DATABASE = "mongodb+srv://rishbro:rishbro@cluster0.telwg.mongodb.net/newon?retryWrites=true&w=majority" #Mongo url
+DATABASE = "mongodb+srv:/xxxxxc@cluster0.telwg.mongodb.net/newon?retryWrites=true&w=majority" #Mongo url
